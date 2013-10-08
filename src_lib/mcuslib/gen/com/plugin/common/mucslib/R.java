f34825082870ac6278a7f8e17f8c3cbeb29251c9
guoqing.sun <guoqing.sun@renren-inc.com>
2013-10-08 11:12:18 +0800

    update

--- src_lib/mcuslib/gen/com/plugin/common/mucslib/R.java
+++ src_lib/mcuslib/gen/com/plugin/common/mucslib/R.java
@@ -68,6 +68,11 @@ containing a value of this type.
              Theme customizations available in newer API levels can go in res/values-vXX/styles.xml, 
 			while customizations related to backward-compatibility can go here.
         
+
+        Base application theme for API 11+. This theme completely replaces
+        AppBaseTheme from res/values/styles.xml on API 11+ devices.
+    
+ API 11 theme customizations can go here. 
          */
         public static int AppBaseTheme=0x7f040000;
         /**  Application theme. 
